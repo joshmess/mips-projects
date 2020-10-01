dacf3412304eba3906e1fd541a5b2bf6a582fbd6
Joshua Messitte <68798981+joshmess@users.noreply.github.com>
2020-10-01 11:07:18 -0700

    Update Commit

--- mips-programs/program_1.asm
+++ mips-programs/program_1.asm
@@ -21,27 +21,27 @@ arrayC: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 .text
 
 main:   
-	la $t1, arrayA  			
-	la $t2, arrayB				
-	la $t3,	arrayC				
+	la $t1, arrayA	#address of arrayA  			
+	la $t2, arrayB	#address of arrayB
+	la $t3,	arrayC	#address of arrayC	
      	
-	li $t4, 0					
-	li $s1, 10					
+	li $t4, 0	#counter					
+	li $s1, 10	#size				
 
 	j loop						
 	
 loop:
-	beq $t4, $s1, reset			
+	beq $t4, $s1, reset	#condition	
 	
-	lw $t5, 0($t1)
-	lw $t6, 0($t2)	
+	lw $t5, 0($t1)	#store val from arrayA
+	lw $t6, 0($t2)	#store val from arrayB
 	
-	sub $t7, $t5, $t6
+	sub $t7, $t5, $t6	#perform sub
 
-	sw $t7, 0($t3) 
+	sw $t7, 0($t3)	#store in arrayC
 
 	
-	addi $t1, $t1, 4
+	addi $t1, $t1, 4	#increment all three arrays and counter
 	addi $t2, $t2, 4
 	addi $t3, $t3, 4
 	addi $t4, $t4, 1
@@ -49,33 +49,33 @@ loop:
 	j loop
 	
 reset:
-	li $t4, 0
+	li $t4, 0		#Reset counter and arrayC pos
 	la $t3, arrayC
 	
-	li $v0, 4			
-	la $a0, result									
+	li $v0, 4		#Print initial string	
+	la $a0, result										
 	syscall 		
 	j print					
 	
 print:
 
-	beq $t4, $s1, final		
+	beq $t4, $s1, final	#condition		
 	
 	lw $s2, 0($t3)				
 
-	li $v0, 1					
+	li $v0, 1	#print value					
 	move $a0, $s2											
 	syscall													
 	
-	li $a0, 32					
+	li $a0, 32	#print space					
 	li $v0, 11										
 	syscall													
 
-	addi $t3, $t3, 4		
+	addi $t3, $t3, 4	#increment pos and counter		
 	addi $t4, $t4, 1		
 
 	j print						
 	
 final:
-	li $v0, 10
+	li $v0, 10	#end program
 	syscall						
