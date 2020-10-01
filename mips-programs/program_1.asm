a8000ee525937ec8e41410c3a6cd40b802029653
Joshua Messitte <68798981+joshmess@users.noreply.github.com>
2020-10-01 10:55:44 -0700

    Add files via upload

--- mips-programs/program_1.asm
+++ mips-programs/program_1.asm
@@ -22,61 +22,61 @@ arrayC: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 					# Array3
 .text
 
 main:   
-	la $t1, arrayA  										# $t1 -> arrayA address
-	la $t2, arrayB											# $t2 -> arrayB address
-	la $t3,	arrayC											# $t3 -> arrayC address
+	la $t1, arrayA  			# $t1 -> arrayA address
+	la $t2, arrayB				# $t2 -> arrayB address
+	la $t3,	arrayC				# $t3 -> arrayC address
      	
-	li $t4, 0												# $t4 -> counter
-	li $s1, 10												# $s1 -> size
+	li $t4, 0					# $t4 -> counter
+	li $s1, 10					# $s1 -> size
 
-	j loop													# jump to loop
+	j loop						# jump to loop
 	
 loop:
-	beq $t4, $s1, reset										# Check -> send to reset
+	beq $t4, $s1, reset			# Check -> send to reset
 	
-	lw $t5, 0($t1)											# store value from arrayA
-	lw $t6, 0($t2)											# store value from arrayB
+	lw $t5, 0($t1)				# store value from arrayA
+	lw $t6, 0($t2)				# store value from arrayB
 	
-	sub $t7, $t5, $t6										# store difference in $t7
+	sub $t7, $t5, $t6			# store difference in $t7
 
-	sw $t7, 0($t3)   										# store differnce in arrayC current spot
+	sw $t7, 0($t3)   			# store differnce in arrayC current spot
 
 	
-	addi $t1, $t1, 4										# increment arrayA
-	addi $t2, $t2, 4										# increment arrayB
-	addi $t3, $t3, 4										# increment arrayC
-	addi $t4, $t4, 1										# increment counter
+	addi $t1, $t1, 4			# increment arrayA
+	addi $t2, $t2, 4			# increment arrayB
+	addi $t3, $t3, 4			# increment arrayC
+	addi $t4, $t4, 1			# increment counter
 	
-	j loop													# jump up
+	j loop						# jump up
 	
 reset:
-	li $t4, 0												# reset counter		
-	la $t3, arrayC											# reset arrayC position
+	li $t4, 0					# reset counter	
+	la $t3, arrayC				# reset arrayC position
 	
-	li $v0, 4												# printing out string
+	li $v0, 4					# printing out string
 	la $a0, result									
 	syscall 		
-	j print													# jump to print
+	j print						# jump to print
 	
 print:
 
-	beq $t4, $s1, final										# check first -> send to final
+	beq $t4, $s1, final			# check first -> send to final
 	
-	lw $s2, 0($t3)											# stro current arrayC val to $s2
+	lw $s2, 0($t3)				# stro current arrayC val to $s2
 
-	li $v0, 1												# print current value
+	li $v0, 1					# print current value
 	move $a0, $s2											
 	syscall													
 	
-	li $a0, 32												# print space
+	li $a0, 32					# print space
 	li $v0, 11										
 	syscall													
 
-	addi $t3, $t3, 4										# increment arracyC position
-	addi $t4, $t4, 1										# increment counter
+	addi $t3, $t3, 4			# increment arracyC position
+	addi $t4, $t4, 1			# increment counter
 
-	j print													# jump up
+	j print						# jump up
 	
 final:
 	li $v0, 10
-	syscall			#end the program
+	syscall						# end the program
